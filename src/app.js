25e59696f01a597d279c5ce9f18c7e143eda862c
YasH-c0der <yasssh2004@gmail.com>
2025-05-31 17:12:44 +0530

    all routers confgured

--- src/app.js
+++ src/app.js
@@ -14,5 +14,11 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// import router
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users", userRouter);
+
+
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -2,35 +2,14 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: "./env"
-})
-import express from "express";
-const app = express()
+    path: "./env",
+});
 
-connectDB();
-
-
-
-
-// ;( async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
-
-//         app.on("error", (error) => {
-//             console.log("Error: ", error);
-//             throw error;
-//         })
-
-//         app.listen(process.env.PORT, () => {
-//             console.log(`App is listening on port: ${process.env.PORT}`);
-            
-//         })
-
-//     } catch (error) {
-//         console.error("Error: ", error);
-//         throw error;
-        
-//     }
-// })()
+connectDB().then(() =>{
+    app.listen(process.env.PORT, () => {
+    console.log(`App is listening on port: ${process.env.PORT}`);
+});
+});
